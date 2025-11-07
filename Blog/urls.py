7d6937803699d7660b40b2a837b46c66f7cdba31
nileena_03 <nileenasanthosh03@gmail.com>
2025-11-07 12:01:24 +0530

    all changes

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,6 +4,8 @@ from .import views
 
 
 urlpatterns = [
-    path('create/',views.create, name='create'),
-    path('list/',views.list, name='list')
+    path('create/', views.create, name='create'),
+    path('list/', views.list, name='list'),
+    path('delete/<id>', views.delete , name='delete'),
+    path('edit/<id>', views.edit , name= 'edit')
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -26,3 +26,21 @@ def create(request):
     else:
         frm = BlogForm()
     return render(request,'create.html',{'frm':frm})
+
+def delete(request, id):
+    instance = bloginfo.objects.get(id=id)
+    instance.delete()
+    blogs_set=bloginfo.objects.all()
+    return render (request, 'list.html', {'posts':blogs_set})
+
+def edit(request,id):
+    instanceToBeEdted = bloginfo.objects.get(id=id)
+    if request.method == 'POST':
+        frm = BlogForm(request.POST,instance=instanceToBeEdted)
+        if frm.is_valid():
+            frm.save()
+            return redirect('list')
+
+    else:
+        frm = BlogForm(instance=instanceToBeEdted)
+    return render(request, 'create.html', {'frm':frm})
